9bd6c2c04e5152f567c4f3895ebd59287601cd0b
LanDev <rbekshenev23@gmail.com>
2023-12-05 09:53:12 +0500

    lesson 3

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 
 
@@ -6,13 +15,37 @@ app = FastAPI(
   title = "Trading App"
 )
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+  return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                      content=jsonable_encoder({"detail": exc.errors()}))
+
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": "admin", "name": ["Bob"]},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
-@app.get('/users/{user_id}')
+class DegreeType(Enum):
+  newbie = "newbie"
+  expert = "expert"
+
+class Degree(BaseModel):
+  id: int
+  created_at: datetime
+  type_degree: DegreeType
+
+
+class User(BaseModel):
+  id: int
+  role: str
+  name: str
+  degree: Optional[List[Degree]] = []
+
+@app.get('/users/{user_id}', response_model=List[User])
 def get_user(user_id: int):
   return [user for user in fake_users if user.get("id") == user_id]
 
@@ -21,19 +54,15 @@ fake_trades = [
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
 
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 1):
-  return fake_trades[offset:][:limit]
+class Trade(BaseModel):
+  id: int
+  user_id: int
+  currency: str = Field(max_length=5)
+  side: str
+  price: float = Field(ge=0)
+  amount: float
 
-
-fake_users2 = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-]
-
-@app.post('/users/{user_id}')
-def change_user_name(user_id: int, new_name: str):
-  current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
-  current_user["name"] = new_name
-  return { "status": 200, "data": current_user }
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+  fake_trades.extend(trades)
+  return {"status": 200, "data": fake_trades}
