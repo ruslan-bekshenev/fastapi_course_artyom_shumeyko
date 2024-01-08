227a11e98435cb83a4d16d72dc93b0c8dffc0197
LanDev <rbekshenev23@gmail.com>
2024-01-09 00:50:46 +0500

    redis init

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 
 from fastapi import Depends, FastAPI
+from fastapi_cache import FastAPICache
 from src.auth.config import fastapi_users
 from src.auth.config import auth_backend, current_user
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as router_operation
+from redis import asyncio as aioredis
+from fastapi_cache.backends.redis import RedisBackend
 
 app = FastAPI(
   title = "Trading App"
@@ -22,3 +25,8 @@ app.include_router(
 )
 
 app.include_router(router_operation)
+
+@app.on_event("startup")
+async def startup_event():
+  redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_responses=True)
+  FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,20 +1,40 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.operations.schemas import OperationCreate
 from src.operations.models import operation
 from src.database import get_async_session
+from fastapi_cache.decorator import cache
+import time
 
 router = APIRouter(
   prefix="/operations",
   tags=["Operations"]
 )
 
+@router.get("/long_operations")
+@cache(expire=30)
+def get_long_op():
+  time.sleep(2)
+  return "Много много данных, которые вычислялись сто лет"
+
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-  query = select(operation).where(operation.c.type == operation_type)
-  result = await session.execute(query)
-  return result.mappings().all()
+  try:
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return {
+      "status": "success",
+      "data": result.mappings().all(),
+      "details": None
+    }
+  except Exception:
+    raise HTTPException(status_code=500, detail={
+      "status": "error",
+      "data": None,
+      "details": None
+    })
+  
 
 @router.post("/")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
